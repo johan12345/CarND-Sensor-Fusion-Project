2faf5f8aa15dc0a26994e39117b73e4ed7caab81
johan12345 <johan.forstner@gmail.com>
2022-03-12 21:51:04 +0100

    implement show_range_image
    ID_S1_EX1

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -11,6 +11,8 @@
 #
 
 # general package imports
+import zlib
+
 import cv2
 import numpy as np
 import torch
@@ -56,21 +58,32 @@ def show_range_image(frame, lidar_name):
 
     ####### ID_S1_EX1 START #######     
     #######
-    print("student task ID_S1_EX1")
-
     # step 1 : extract lidar data and range image for the roof-mounted lidar
+    lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0]
+
+    ri = dataset_pb2.MatrixFloat()
+    ri.ParseFromString(
+        zlib.decompress(lidar.ri_return1.range_image_compressed))
+    ri = np.array(ri.data).reshape(ri.shape.dims)
     
     # step 2 : extract the range and the intensity channel from the range image
+    range = ri[:, :, 0]
+    intensity = ri[:, :, 1]
     
     # step 3 : set values <0 to zero
+    range[range < 0] = 0
+    intensity[intensity < 0] = 0
     
     # step 4 : map the range channel onto an 8-bit scale and make sure that the full range of values is appropriately considered
+    range_8bit = (range * 255 / range.max()).astype(np.uint8)
     
     # step 5 : map the intensity channel onto an 8-bit scale and normalize with the difference between the 1- and 99-percentile to mitigate the influence of outliers
+    pctls = np.percentile(intensity, [1, 99])
+    intensity_8bit = (intensity - pctls[0]) / (pctls[1] - pctls[0]) * 255
+    intensity_8bit = intensity_8bit.astype(np.uint8)
     
     # step 6 : stack the range and intensity image vertically using np.vstack and convert the result to an unsigned 8-bit integer
-    
-    img_range_intensity = [] # remove after implementing all steps
+    img_range_intensity = np.vstack([range_8bit, intensity_8bit])
     #######
     ####### ID_S1_EX1 END #######     
     
